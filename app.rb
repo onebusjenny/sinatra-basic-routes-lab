67cda536d3f992ee9a29571be38adee76d065863
Jenny Chu <jchudesign0930@gmail.com>
2019-10-24 21:17:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,5 +6,19 @@ class App < Sinatra::Base
   get '/' do
     "Hello, World!"
   end
+  
+  get '/name' do
+    
+  end
+  
+  get '/hometown' do
+  
+  end
+  
+  get '/favorite-song' do
+    
+  end
 
+
+  
 end
