bb6b3d336331d538a9aa84b5ac56152034844e24
Jenny Chu <jchudesign0930@gmail.com>
2019-10-24 21:18:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Jenny"
+  end
+  
+  get '/hometown' do
+    "My hometown is Taiwan"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Apologize"
+  end
+
+
+  
 end
